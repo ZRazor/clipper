0ea7de4101fac1429ba7312a9adde99c73f2be02
Ivan Kalita <input.txt@hotmail.com>
2016-07-15 18:43:56 +1000

    Добавил в аудио-граф питч-шифтер

--- clipperSources/Utils/PostProcess/MKRAudioProcessor.m
+++ clipperSources/Utils/PostProcess/MKRAudioProcessor.m
@@ -15,16 +15,17 @@ OSStatus OSSTATUS = noErr;
 @interface MKRAudioProcessor()
 
 @property (nonatomic) AUGraph graph;
-//@property (nonatomic) AudioUnit ioUnit;
 @property (nonatomic) AudioUnit GIOUnit;
 @property (nonatomic) AudioUnit mixerUnit;
 @property (nonatomic) AudioUnit originalPlayerUnit;
 @property (nonatomic) AudioUnit playbackPlayerUnit;
-//@property (nonatomic) AUNode ioNode;
+@property (nonatomic) AudioUnit timePitchUnit;
+
 @property (nonatomic) AUNode GIONode;
 @property (nonatomic) AUNode mixerNode;
 @property (nonatomic) AUNode originalPlayerNode;
 @property (nonatomic) AUNode playbackPlayerNode;
+@property (nonatomic) AUNode timePitchNode;
 
 @property (nonatomic) Float64 sampleRate;
 @property (nonatomic) SInt32 framesPerSlice;
@@ -99,10 +100,18 @@ OSStatus OSSTATUS = noErr;
     GIODesc.componentFlagsMask = 0;
     GIODesc.componentManufacturer = kAudioUnitManufacturer_Apple;
     
+    AudioComponentDescription timePitchDesc;
+    timePitchDesc.componentType = kAudioUnitType_FormatConverter;
+    timePitchDesc.componentSubType = kAudioUnitSubType_NewTimePitch;
+    timePitchDesc.componentFlags = 0;
+    timePitchDesc.componentFlagsMask = 0;
+    timePitchDesc.componentManufacturer = kAudioUnitManufacturer_Apple;
+
     OSSTATUS = AUGraphAddNode(self.graph, &originalPlayerDesc, &_originalPlayerNode); OSSTATUS_CHECK
     OSSTATUS = AUGraphAddNode(self.graph, &playbackPlayerDesc, &_playbackPlayerNode); OSSTATUS_CHECK
     OSSTATUS = AUGraphAddNode(self.graph, &mixerDesc, &_mixerNode); OSSTATUS_CHECK
     OSSTATUS = AUGraphAddNode(self.graph, &GIODesc, &_GIONode); OSSTATUS_CHECK
+    OSSTATUS = AUGraphAddNode(self.graph, &timePitchDesc, &_timePitchNode); OSSTATUS_CHECK
     
     OSSTATUS = AUGraphOpen(self.graph); OSSTATUS_CHECK
     
@@ -110,11 +119,14 @@ OSStatus OSSTATUS = noErr;
     OSSTATUS = AUGraphNodeInfo(self.graph, self.playbackPlayerNode, NULL, &_playbackPlayerUnit); OSSTATUS_CHECK
     OSSTATUS = AUGraphNodeInfo(self.graph, self.mixerNode, NULL, &_mixerUnit); OSSTATUS_CHECK
     OSSTATUS = AUGraphNodeInfo(self.graph, self.GIONode, NULL, &_GIOUnit); OSSTATUS_CHECK
+    OSSTATUS = AUGraphNodeInfo(self.graph, self.timePitchNode, NULL, &_timePitchUnit); OSSTATUS_CHECK
     
-    OSSTATUS = AUGraphConnectNodeInput(self.graph, self.originalPlayerNode, 0, self.mixerNode, 0); OSSTATUS_CHECK
+    OSSTATUS = AUGraphConnectNodeInput(self.graph, self.originalPlayerNode, 0, self.timePitchNode, 0); OSSTATUS_CHECK
+    OSSTATUS = AUGraphConnectNodeInput(self.graph, self.timePitchNode, 0, self.mixerNode, 0); OSSTATUS_CHECK
     OSSTATUS = AUGraphConnectNodeInput(self.graph, self.playbackPlayerNode, 0, self.mixerNode, 1); OSSTATUS_CHECK
     OSSTATUS = AUGraphConnectNodeInput(self.graph, self.mixerNode, 0, self.GIONode, 0); OSSTATUS_CHECK
     
+    //setup mixer
     UInt32 busCount = 2;
     OSSTATUS = AudioUnitSetProperty(self.mixerUnit, kAudioUnitProperty_ElementCount, kAudioUnitScope_Input, 0, &busCount, sizeof(busCount)); OSSTATUS_CHECK
     
@@ -250,6 +262,12 @@ OSStatus OSSTATUS = noErr;
     
     int totalFrames = self.maxSampleTime;
     while (totalFrames > 0) {
+        
+//        if (totalFrames / numberFrames > 26880 / 1000 * self.sampleRate && totalFrames / numberFrames < 38400 / 1000 * self.sampleRate) {
+//            OSSTATUS = AudioUnitSetParameter(self.timePitchUnit, kNewTimePitchParam_Pitch, kAudioUnitScope_Global, 0, (i += 10) % 2400, 0); OSSTATUS_CHECK
+//        } else if (totalFrames / numberFrames > 38400 / 1000 * self.sampleRate) {
+//            OSSTATUS = AudioUnitSetParameter(self.timePitchUnit, kNewTimePitchParam_Pitch, kAudioUnitScope_Global, 0, 0, 0); OSSTATUS_CHECK
+//        }
         if (totalFrames < numberFrames) {
             numberFrames = totalFrames;
         } else {
@@ -276,13 +294,5 @@ OSStatus OSSTATUS = noErr;
     
     completionBlock([NSURL fileURLWithPath:dstFilePath]);
 }
-
-//- (NSString *)filePath {
-//    NSArray *documentsFolders =
-//    NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-//    NSString *fileName = [NSString stringWithFormat:@"%@.m4a", [[NSUUID UUID] UUIDString]];
-//    NSString *path = [documentsFolders[0] stringByAppendingPathComponent:fileName];
-//    return path;
-//}
 
 @end
