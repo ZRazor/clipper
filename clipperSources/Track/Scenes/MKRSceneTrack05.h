aae73432c29cb9d61f17af08973ef6fd04ea2bd7
Ivan Kalita <input.txt@hotmail.com>
2016-07-20 19:18:35 +1000

    Добавил 5-й трек, удалил 1-й и 2-й

---
+++ clipperSources/Track/Scenes/MKRSceneTrack05.h
@@ -0,0 +1,14 @@
+//
+//  MKRSceneTrack05.h
+//  clipper
+//
+//  Created by dev on 20.07.16.
+//  Copyright © 2016 mayak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MKRScene.h"
+
+@interface MKRSceneTrack05 : MKRScene
+
+@end
